b4abdc05ee724a6c924d380243ec39f8679e5ad9
senthildeveloper <senthildeveloper4@gmail.com>
2023-11-06 16:11:45 +0530

    footer linked

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -6,8 +6,8 @@ const Contact = () => {
       <form action='https://getform.io/f/e4a4af45-2975-4f24-ab37-35ad9eb37113' method='POST' className='flex flex-col h-full max-w-[600px] w-full'>
           <div className='pt-8'>
             <p className='text-4xl text-white font-bold inline border-b-4 border-[#f83bff]'>Contact</p>
-            <p className='text-gray-300 py-4'>Submit the form below or shoot me<a  className=' w-[100px] text-blue-600  
-                ' href="mailto:senthilk2003k@gmail.com"> E-mail  </a> </p>
+            <p className='text-gray-300 py-4'>Submit the form below or shoot me
+            <a  className='w-[100px] text-blue-600' href="mailto:senthilk2003k@gmail.com"> E-mail  </a> </p>
           </div>
           <input  className="bg-[#ccd6f6] p-2"type="text" placeholder='Name' name='user_name' required/>
           <input className='my-4  bg-[#ccd6f6] p-2' type="email" placeholder='E-mail' name='user_email' required/>
--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ const Home = () => {
            <div className='max-w-[1000px] mx-auto px-8  flex flex-col justify-center h-full text-white'>
               <p className=' text-[#f83bff]  '>Hi, my name is </p>
               <h1 className='text-4xl sm:text-5xl text-gray-200 font-bold '>SENTHIL</h1>
-              <h2 className='text-4xl sm:text-5xl font-bold text-[#8892b0]'> I'm a <span class='bg-[#8892b080] text-gray-300'>FRONTEND</span> Developer.</h2>
+              <h2 className='text-4xl sm:text-5xl font-bold text-[#8892b0]'> I'm a <span className='bg-[#8892b080] text-gray-300'>FRONTEND</span> Developer.</h2>
               <p className='text-[#8892b0] py-4 max-w-[700px]' >I'm a FRONTEND Developer specializing in building (and occasionally designing) expectional digital experiences. Currently, I'm focused on building responsive FRONTEND web applications </p>
            
            <div className='flex '>
