a8f98bb0c80859794c0714a52b0864fbb7c7f7f3
senthil <131505586+senthil-developer@users.noreply.github.com>
2023-11-06 17:09:48 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 import Link from 'next/link'
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+    <div name='home' className='w-full h-screen '>
            {/* Container */}
            <div className='max-w-[1000px] mx-auto px-8  flex flex-col justify-center h-full text-white'>
               <p className=' text-[#f83bff]  '>Hi, my name is </p>
